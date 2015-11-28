1e3a6e16076126d3af5f9e90fb46739f29da2228
varalakshmi <vara2504@gmail.com>
2015-11-29 02:13:02 +0530

    map changes

--- client/templates/RapidResponseDashboard/LiveVictimMap.js
+++ client/templates/RapidResponseDashboard/LiveVictimMap.js
@@ -4,28 +4,96 @@ Template.LiveVictimMap.helpers({
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // Map initialization options
-      loc = livelocation.find();
-      var lat =loc.lat;
-      var lon = loc.lon;
-     // alert(lat);
-      //alert(lon);
+     //   livelocation.insert({"lat":"12.6","lon":"80.1","doneflag":"true"});
+     //   livelocation.insert({"lat":"11.6","lon":"79.7","doneflag":"true"});
+     // livelocation.insert({"lat":"13.6","lon":"79.7","doneflag":"false"});
+     // livelocation.insert({"lat":"13.3","lon":"79.7","doneflag":"false"});
+
+
+
       return {
-        center: new google.maps.LatLng(12.6, 80.1),
+        center: new google.maps.LatLng(12.6, 75.1),
         zoom: 8
       };
     }
   }
 });
 
+
+
+var content = '<button type=submit id="CompleteButton" >Mark as Completed</button>';
+
+
+
 Template.LiveVictimMap.onCreated(function() {
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('exampleMap', function(map) {
-    // Add a marker to the map once it's ready
-   
 
+  
+  var markers  ={};
+livelocation.find({"doneflag":"false"}).observe({ 
+
+ added: function(document) {
+    // Create a marker for this document
     var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
+      draggable: false,
+      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng(document.lat, document.lon),
+      map: map.instance,
+      clickable:true,
+      // We store the document _id on the marker in order 
+      // to update the document within the 'dragend' event below.
+      id: document._id
     });
-  });
+
+
+    // This listener lets us drag markers on the map and update their corresponding document.
+    google.maps.event.addListener(marker, 'dragend', function(event) {
+      livelocation.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lon() }});
+    });
+
+    // Store this marker instance within the markers object.
+    markers[document._id] = marker;
+  },
+  changed: function(newDocument, oldDocument) {
+    markers[newDocument._id].setPosition({ lat: newDocument.lat, lon: newDocument.lon });
+  },
+  removed: function(oldDocument) {
+    // Remove the marker from the map
+    markers[oldDocument._id].setMap(null);
+
+    // Clear the event listener
+    google.maps.event.clearInstanceListeners(
+      markers[oldDocument._id]);
+
+    // Remove the reference to this marker instance
+    delete markers[oldDocument._id];
+  }
 });
+
+
+    // Add a marker to the map once it's ready
+    //   loc = livelocation.find().fetch();
+      
+    //   for(i =0;i<loc.length;i++){
+    //    var lat =loc[i].lat;
+    //     var lon = loc[i].lon;
+      
+    //     var marker = new google.maps.Marker({
+    //       draggable:true,
+    //       animation: google.maps.Animation.DROP,
+    //       position: new google.maps.LatLng(lat,lon),
+    //       map: map.instance,
+    //       id:loc.id
+    //     });
+    // }
+
+
+
+  });
+
+
+
+});
+
+
